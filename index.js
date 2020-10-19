a6c5241fdbb5d77096ebce2dd9afc1f6f73c4966
wasuaje <wasuaje@gmail.com>
2020-10-19 19:56:38 -0300

    1 file modified

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Spritesheet from 'react-responsive-spritesheet';
-import './style.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Spritesheet from "react-responsive-spritesheet";
+import "./style.css";
+import Ship from "./Ship.png";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'React'
+      name: "React"
     };
   }
 
   render() {
     return (
       <div className="rrs-container">
-        <img src="https://raw.githubusercontent.com/danilosetra/react-responsive-spritesheet/master/assets/images/logo/rrs-logo.png" width="250" height="250" />
+        <img
+          src="https://raw.githubusercontent.com/danilosetra/react-responsive-spritesheet/master/assets/images/logo/rrs-logo.png"
+          width="250"
+          height="250"
+        />
         <hr />
 
         <Spritesheet
           className={`my-element__class--style`}
-          image={`https://www.spriters-resource.com/resources/sheets/93/96603.png?updated=1506289701`}
+          image={Ship}
           widthFrame={20}
-          heightFrame={40}          
+          heightFrame={40}
           fps={20}
           steps={15}
           startAt={1}
           endAt={260}
-          autoplay={false}          
+          autoplay={false}
           loop={true}
           onMouseEnter={spritesheet => {
             spritesheet.play();
@@ -40,21 +45,34 @@ class App extends Component {
         <p>
           image source:
           <br />
-          <a href="https://www.spriters-resource.com/resources/sheets/133/136347.png?updated=1595882136" target="_blank">
-            <img src="https://www.spriters-resource.com/resources/sheets/93/96603.png?updated=1506289701" width="40%" />
+          <a
+            href="https://www.spriters-resource.com/resources/sheets/133/136347.png?updated=1595882136"
+            target="_blank"
+          >
+            <img
+              src="https://www.spriters-resource.com/resources/sheets/93/96603.png?updated=1506289701"
+              width="40%"
+            />
             <small>&#x2197;</small>
           </a>
         </p>
         <hr />
         <p>
-          <a href="https://stackblitz.com/edit/react-responsive-spritesheet-example-02?file=index.js">edit source code<small>&#x2197;</small></a>
+          <a href="https://stackblitz.com/edit/react-responsive-spritesheet-example-02?file=index.js">
+            edit source code<small>&#x2197;</small>
+          </a>
           <br />
           <br />
-          <a href="https://github.com/danilosetra/react-responsive-spritesheet" target="_blank">see docs and other examples on GitHub <small>&#x2197;</small></a>
+          <a
+            href="https://github.com/danilosetra/react-responsive-spritesheet"
+            target="_blank"
+          >
+            see docs and other examples on GitHub <small>&#x2197;</small>
+          </a>
         </p>
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
